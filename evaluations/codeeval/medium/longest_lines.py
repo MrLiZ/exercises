848218b9b884acc2345dd18a48a9dcc8daf9dfa0
Alberto <amegia@savemeeting.com>
2014-02-23 18:37:54 +0100

    Largest lines challenge solved

---
+++ evaluations/codeeval/medium/longest_lines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+
+import heapq
+import traceback
+import sys
+
+
+if __name__ == "__main__":
+
+    arg = sys.argv[1]
+    heap = []
+    try:
+        with open(arg, "r") as f:
+            nlines = int(f.readline())
+            for line in f:
+                if line:
+                    heapq.heappush(heap, (len(line), line.strip()))
+    except IOError:
+        print(traceback.format_exc())
+
+    nlargest = heapq.nlargest(nlines, heap)
+    for elem in nlargest:
+        print(elem[1])
+
