23f0f7dc10d9db652e8c688bc96d36cd7b0c175d
Alberto <amegia@savemeeting.com>
2014-02-28 23:19:40 +0100

    Pass triangle challenge solved

--- evaluations/codeeval/medium/pass_triangle.py
+++ evaluations/codeeval/medium/pass_triangle.py
@@ -7,17 +7,15 @@ import traceback
 import heapq
 
 
-MIN = -9999
-
-
-def triangle(tree):
-    """Using a min heap with a max problem"""
-    h = len(tree[0]) - 1
+def triangle_bandb(tree):
+    """Branch and bound approach (up to 34 rows)"""
+    MIN = -9999 # maximization problem with min heap
+    h = len(tree) - 1
     best = float("inf")
-    triangle = []
-    heapq.heappush(triangle, (-tree[i][j], (0, 0)))
+    heap = []
+    heapq.heappush(heap, (-tree[0][0], 0, 0))
     while heap:
-        points, (i, j) = heapq.heappop(triangle)
+        points, i, j = heapq.heappop(heap)
         if i == h and points < best:
             best = points
         else:
@@ -26,22 +24,36 @@ def triangle(tree):
                 continue
             down = points + -tree[i+1][j]
             down_right = points + -tree[i+1][j+1]
-            heapq.heappush(triangle, (down, (i+1, j))
-            heapq.heappush(triangle, (down_right, (i+1, j+1))
-    return best
+            heapq.heappush(heap, (down, i+1, j))
+            heapq.heappush(heap, (down_right, i+1, j+1))
+
+    return -best
+
+
+def triangle_dp(tree):
+    """Dinamic programming approach"""
+    h = len(tree)
+    res = [0] * h
+    res[h - 1] = tree[h - 1]
+    for i in xrange(h - 2, -1, -1):
+        row_length = len(tree[i])
+        res[i] = [0] * row_length
+        for j in xrange(row_length - 1, -1, -1):
+            res[i][j] = tree[i][j] + max(res[i+1][j], res[i+1][j+1])
+
+    return res[0][0] 
 
 
 if __name__ == "__main__":
 
     arg = sys.argv[1]
     tree = []
-    try
+    try:
         with open(arg, "r") as f:
-            i = 0
             for line in f:
-                tree[i] = line.strip().split()
-                i += 1
+                tree.append(map(int, line.strip().split()))
+                #tree.append([int(x) for x in line.strip().split()])
     except IOError:
         print(traceback.format_exc())
 
-    print(triangle(tree))
+    print(triangle_dp(tree))
